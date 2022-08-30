264956026962abf72c16369d955b21254cc83d98
xauvk <xauvkubalexanderyang@live.com>
2022-08-30 10:45:27 -0400

    commit tree be8aabd3f8e15e14f2d1aecef3b08279bfa4b466

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,12 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    render json: Student.all.find(params[:id])
+  end
+
+  def match
+    render json: Student.all.where(first_name: params[:key1])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
+  get '/students/:id', to: 'students#show'
+  get '/students?key1=value1&key2=value2', to: 'students#match'
 end
